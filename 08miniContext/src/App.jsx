b955b2c88ac0f17b4549a88fd5301d93d0fbe1c9
Vivek Kumar <vivekbarwal1982@gmail.com>
2025-12-21 01:15:38 +0530

    contextApi project

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>Hii</>
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
